526297e84f74bf0fa7ec8dd1a6a5b1a0bcc90ad4
isaacn123 <nsambai72@gmail.com>
2022-07-18 22:50:27 +0300

    another commit add register function

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        // Force SSL in production
+if ($this->app->environment() == 'production') {
+    URL::forceScheme('https');
+}
     }
+
+
+
 }
