961f20e693160e7da01ea46137f64e6b2b34bd33
isaacn123 <nsambai72@gmail.com>
2022-07-29 19:33:30 +0300

    agent detail api.

--- app/Http/Controllers/AgentAuthController.php
+++ app/Http/Controllers/AgentAuthController.php
@@ -51,6 +51,7 @@ class AgentAuthController extends Controller
         // );
 
             $data = [
+                $resp,
             "agent_id" => $resp->agent_id,
             "agent_name" => $resp->fullName,
             "agent_contact" => $resp->phoneNumber,
